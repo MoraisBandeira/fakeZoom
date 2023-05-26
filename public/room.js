76f5abb89328da3ac308b76b016cc91442653e25
anderson.morais <anderson.morais@hemoce.ce.gov.brgit config --global user.email anderson.morais@hemoce.ce.gov.br>
2023-05-25 21:58:58 -0300

    criação de salas e passagem de texto entre elas

--- public/room.js
+++ public/room.js
@@ -1,1 +1,32 @@
 const socket = io();
+
+const urlSearch =  new URLSearchParams(window.location.search);
+const username = urlSearch.get('user');
+const room = urlSearch.get('room');
+
+
+socket.emit("access_room",{
+   username,
+   room
+})
+
+
+document
+.getElementById('floatingTextarea2')
+.addEventListener('keyup',event=>{
+   const message = event.target.value;
+   const data = {
+       room,
+       message,
+       username
+   }
+
+
+   socket.emit('message',data)
+})
+
+
+socket.on('message',data=>{
+   document
+   .getElementById('floatingTextarea2').value = data.text
+})
--- src/websocket.js
+++ src/websocket.js
@@ -1,5 +1,45 @@
 const { io } = require('./app.js');
 
+const users = [];
+const messages = [];
+
+
 io.on('connection',(socket)=>{
-    console.log(socket.id)
-})
+   socket.on('access_room',data=>{
+      
+       socket.join(data.room);
+
+
+       const userInRoom = users.find(user=> user.username === data.username && user.room === data.room);
+
+
+       if(userInRoom){
+           userInRoom.socket_id = socket_id
+       }else{
+        users.push({
+            room:data.room,
+            username:data.username,
+            socket_id:socket.id
+            })
+        }
+    })
+ 
+ 
+    socket.on('message',data=>{
+ 
+ 
+        messages.push({
+            room:data.room,
+            username:data.username,
+            text:data.message
+        })
+ 
+ 
+        io.to(data.room).emit('message', {
+            text:data.message
+        });
+ 
+ 
+    })
+ })
+ 
